588c239b29ddd7bead05cfeef8367ac5c915d1d2
YiHwanRyu <yihwandev@gmail.com>
2023-12-03 16:40:48 +0900

    [UPDATE] 클래스 분리 후 정리

--- Calculator/app/src/main/java/com/android/calculator/Main.kt
+++ Calculator/app/src/main/java/com/android/calculator/Main.kt
@@ -1,10 +1,22 @@
 package com.android.calculator
+// 사용할 클래스
+val setCalculation = SetCalculation()
 
 fun main() {
+    //반복 계산 수행(독립 계산)
     while(true) {
-        var num1 = readLine()!!.toDouble()
-        var operator = readLine()!!
-        var num2 = readLine()!!.toDouble()
-        var calculator = Calculator(num1, num2, operator)
+
+        var num1 = setCalculation.setNumber()
+
+        var calculator = Calculator(setCalculation.setOperator())
+
+        var num2 = setCalculation.setNumber()
+
+        // 계산 결과 출력
+        println(calculator.calculate(num1, num2))
     }
 }
+
+
+
+
