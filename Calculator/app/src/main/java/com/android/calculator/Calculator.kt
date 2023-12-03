ce9d88a829582afbcc097e1a24192fbd878449e4
YiHwanRyu <yihwandev@gmail.com>
2023-12-03 16:40:26 +0900

    [UPDATE] 클래스 분리

--- Calculator/app/src/main/java/com/android/calculator/Calculator.kt
+++ Calculator/app/src/main/java/com/android/calculator/Calculator.kt
@@ -1,21 +1,8 @@
 package com.android.calculator
 
-class Calculator {
-    var operator: String = ""
-    var num1: Double = 0.0
-    var num2: Double = 0.0
-    constructor(_num1:Double, _num2:Double, _operator:String) {
-        num1 = _num1
-        num2 = _num2
-        operator = _operator
-        when(operator) {
-            "+" -> println(num1 + num2)
-            "-" -> println(num1 - num2)
-            "*" -> println(num1 * num2)
-            "/" -> println(num1 / num2)
-            "%" -> println(num1 % num2)
-            else -> println("잘못된 연산자 입력입니다.")
-        }
+class Calculator(private val operation: AbstractOperation) {
+    fun calculate(num1:Double, num2:Double) : Double {
+        return operation.operate(num1, num2)
     }
 
 }
